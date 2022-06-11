d269a906a4475e24741ea3d8ff5c8bf12a57b677
JEREMY CHOPPE <JEREMY.CHOPPE@UNIL.CH>
2022-06-11 15:28:57 +0200

    k

--- src/app.py
+++ src/app.py
@@ -1,10 +1,6 @@
 # Standard imports
 import pandas as pd
 
-# matplotlib
-import matplotlib as mpl
-import matplotlib.pyplot as plt
-import seaborn as sns
 #plotly
 import plotly.express as px
 import plotly.graph_objects as go
@@ -21,10 +17,6 @@ if st.sidebar.checkbox('Show dataframe'):
     st.header("dataframe")
     st.dataframe(df.head())
 
-# Then the radio botton for the plot type
-show_plot = st.sidebar.radio(
-    label='Choose Plot type', options=['Matplotlib', 'Plotly'])
-
 st.header("Highway Fuel Efficiency")
 years = ["All"]+sorted(pd.unique(df['year']))
 year = st.sidebar.selectbox("choose a Year", years)   # Here the selection of the year.
@@ -35,29 +27,6 @@ show_means = st.sidebar.radio(
     label='Show Class Means', options=['Yes', 'No'])
 
 st.subheader(f'Fuel efficiency vs. engine displacement for {year}')
-
-
-# With these functions we wrangle the data and plot it.
-def mpg_mpl(year, car_class, show_means):
-    fig, ax = plt.subplots()
-    if year == 'All':
-        group = df
-    else:
-        group = df[df['year'] == year]
-    if car_class != 'All':
-        st.text(f'plotting car class: {car_class}')
-        group = group[group['class'] == car_class]
-    group.plot('displ', 'hwy', marker='.', linestyle='', ms=12, alpha=0.5, ax=ax, legend=None)
-    if show_means == "Yes":
-        means = df.groupby('class').mean()
-        for cc in means.index:
-            ax.plot(means.loc[cc, 'displ'], means.loc[cc, 'hwy'], marker='.', linestyle='', ms=12, alpha=1, label=cc)
-        ax.legend(loc='upper right', bbox_to_anchor=(1.1, 1))
-    ax.set_xlim([1, 8])
-    ax.set_ylim([10, 50])
-    plt.close()
-    return fig
-
 
 def mpg_plotly(year, car_class, show_means):
     if year == 'All':
@@ -75,9 +44,6 @@ def mpg_plotly(year, car_class, show_means):
     return fig
   
 
-if show_plot == 'Plotly':
-    st.plotly_chart(mpg_plotly(year, car_class, show_means))
-    
-else:
-    st.pyplot(mpg_mpl(year, car_class, show_means))
+st.plotly_chart(mpg_plotly(year, car_class, show_means))
+
 
